0dfdbd637cd47df6f1207c97c8507cdae2238b8f
nicolasfrechette <nicolasfrechette91@gmail.com>
2020-06-13 16:15:11 -0400

    fixed jquery for airplane page

--- airplane/airplane.js
+++ airplane/airplane.js
@@ -2,28 +2,43 @@ $(document).ready(function(){
    $('#montrealButton').click(function() {
      $('.toronto').hide();
      $('.edmonton').hide();
-     $('.montreal').show();
-     $(this).css('background-color', '#eb4034');
-     $('#torontoButton').css('background-color', '');
-     $('#edmontonButton').css('background-color', '');
+     if ($('#montrealButton').css('background-color') === 'rgb(235, 64, 52)') {
+        $('.montreal').hide();
+        $('#montrealButton').css('background-color', '');
+     } else {
+        $('.montreal').show();
+        $(this).css('background-color', '#eb4034');
+        $('#torontoButton').css('background-color', '');
+        $('#edmontonButton').css('background-color', '');
+     }
    });
 
    $('#torontoButton').click(function() {
      $('.montreal').hide();
      $('.edmonton').hide();
-     $('.toronto').show();
-     $(this).css('background-color', '#eb4034');
-     $('#montrealButton').css('background-color', '');
-     $('#edmontonButton').css('background-color', '');
+     if ($('#torontoButton').css('background-color') === 'rgb(235, 64, 52)') {
+         $('.toronto').hide();
+         $('#torontoButton').css('background-color', '');
+     } else {
+         $('.toronto').show();
+         $(this).css('background-color', '#eb4034');
+         $('#montrealButton').css('background-color', '');
+         $('#edmontonButton').css('background-color', '');
+     }
    });
 
    $('#edmontonButton').click(function() {
      $('.montreal').hide();
      $('.toronto').hide();
-     $('.edmonton').show();
-     $(this).css('background-color', '#eb4034');
-     $('#montrealButton').css('background-color', '');
-     $('#torontoButton').css('background-color', '');
+     if ($('#edmontonButton').css('background-color') === 'rgb(235, 64, 52)') {
+        $('.edmonton').hide();
+        $('#edmontonButton').css('background-color', '');
+     } else {
+         $('.edmonton').show();
+         $(this).css('background-color', '#eb4034');
+         $('#montrealButton').css('background-color', '');
+         $('#torontoButton').css('background-color', '');
+     }
    });
 
      var language = $('#page').text();
