f852fb6c3f17c9b86c3883f7de8881f5d25043b9
nicolasfrechette <nicolasfrechette91@gmail.com>
2020-06-14 18:27:59 -0400

    added the main menu button to the page contact us

--- contactUs/contactUs.js
+++ contactUs/contactUs.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $("#email").bind('click', function () {
        window.location.href = $(this).attr('href');
     });
+
+    var language = $('#page').text();
+
+    $(".imagePrevPage").click(function() {
+      window.location.href = "../hotel/hotel" + language + ".html";
+    });
 });
