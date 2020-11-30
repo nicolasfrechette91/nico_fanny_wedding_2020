d9e3674277ba90f570321aad7ee76c2018b982fa
nicolasfrechette <nicolasfrechette91@gmail.com>
2020-11-29 21:10:22 -0500

    cheesyStuff world section beginning

--- cheesyStuff/cheesyStuff.js
+++ cheesyStuff/cheesyStuff.js
@@ -9,11 +9,13 @@ $(document).ready(function(){
 
     showHideMobile(isMobile);
 
+    generateMap();
+
    $('#timelineButton').click(function() {
      $('.recipe').hide();
      $('.world').hide();
      if ($('#timelineButton').css('background-color') === 'rgb(235, 64, 52)') {
-        $('.prevNextButtons').css('padding-top', '300px');
+        $('.prevNextButtons').css('padding-top', '200px');
         $('.timelineContainer').hide();
         $('#timelineButton').css('background-color', '');
      } else {
@@ -26,7 +28,7 @@ $(document).ready(function(){
    });
 
    $('#recipeButton').click(function() {
-     $('.prevNextButtons').css('padding-top', '300px');
+     $('.prevNextButtons').css('padding-top', '200px');
      $('.timelineContainer').hide();
      $('.world').hide();
      if ($('#recipeButton').css('background-color') === 'rgb(235, 64, 52)') {
@@ -41,13 +43,14 @@ $(document).ready(function(){
    });
 
    $('#worldButton').click(function() {
-     $('.prevNextButtons').css('padding-top', '300px');
      $('.timelineContainer').hide();
      $('.recipe').hide();
      if ($('#worldButton').css('background-color') === 'rgb(235, 64, 52)') {
+        $('.prevNextButtons').css('padding-top', '200px');
         $('.world').hide();
         $('#worldButton').css('background-color', '');
      } else {
+         $('.prevNextButtons').css('padding-top', '0px');
          $('.world').show();
          $(this).css('background-color', '#eb4034');
          $('#timelineButton').css('background-color', '');
@@ -80,6 +83,8 @@ $(document).ready(function(){
         }
      });
 
+//    $('#world-map').vectorMap({map: 'world_mill'});
+
      var language = $('#page').text();
 
      $(".imagePrevPage").click(function() {
@@ -98,4 +103,51 @@ function showHideMobile(isMobile){
       } else {
         $('.mobile').hide();
     }
+}
+
+function generateMap(){
+  $('#world-map-markers').vectorMap({
+    map: 'world_mill',
+    scaleColors: ['#C8EEFF', '#0071A4'],
+    normalizeFunction: 'polynomial',
+    hoverOpacity: 0.7,
+    hoverColor: false,
+    markerStyle: {
+      initial: {
+        fill: '#F8E23B',
+        stroke: '#383f47'
+      }
+    },
+    backgroundColor: '#383f47',
+    markers: [
+      {latLng: [41.90, 12.45], name: 'Vatican City'},
+      {latLng: [43.73, 7.41], name: 'Monaco'},
+      {latLng: [-0.52, 166.93], name: 'Nauru'},
+      {latLng: [-8.51, 179.21], name: 'Tuvalu'},
+      {latLng: [43.93, 12.46], name: 'San Marino'},
+      {latLng: [47.14, 9.52], name: 'Liechtenstein'},
+      {latLng: [7.11, 171.06], name: 'Marshall Islands'},
+      {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
+      {latLng: [3.2, 73.22], name: 'Maldives'},
+      {latLng: [35.88, 14.5], name: 'Malta'},
+      {latLng: [12.05, -61.75], name: 'Grenada'},
+      {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
+      {latLng: [13.16, -59.55], name: 'Barbados'},
+      {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
+      {latLng: [-4.61, 55.45], name: 'Seychelles'},
+      {latLng: [7.35, 134.46], name: 'Palau'},
+      {latLng: [42.5, 1.51], name: 'Andorra'},
+      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
+      {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
+      {latLng: [1.3, 103.8], name: 'Singapore'},
+      {latLng: [1.46, 173.03], name: 'Kiribati'},
+      {latLng: [-21.13, -175.2], name: 'Tonga'},
+      {latLng: [15.3, -61.38], name: 'Dominica'},
+      {latLng: [-20.2, 57.5], name: 'Mauritius'},
+      {latLng: [26.02, 50.55], name: 'Bahrain'},
+      {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
+    ]
+  });
+
+  $('.world').hide();
 }
